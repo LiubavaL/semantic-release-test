0251e0948dfd0d9f1720673d023b08c97fb5d8e6
LiubavaL <liuba.vitko@gmail.com>
2025-06-07 19:01:39 +0000

    feat: add minus util

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ export function sum(a:number, b:number){
     return a + b;
 }
 
+export function minus(a:number, b:number){
+    return a - b;
+}
+
 export function multiply(a:number, b:number){
     return a * b;
 }
